204bff6f253ee52d04c606312f22695543fd7fab
Razvan Luta <razvan.luta@gmail.com>
2020-06-18 01:52:13 -0700

    Initialized repository.

---
+++ Editor/TexturePacker.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+
+namespace UnityTextureRgbPacker.Editor
+{
+    public class TexturePacker
+    {
+        public static UnityEngine.Texture2D GetCompositeTextureRgb(
+            Texture2D textureRedChannel,
+            Texture2D textureGreenChannel,
+            Texture2D textureBlueChannel,
+            string compositeTextureName)
+        {
+            Texture2D packedTexture;
+
+            if (!textureRedChannel || !textureGreenChannel || !textureBlueChannel)
+            {
+                throw new System.Exception("Input textures cannot be null.");
+            }
+
+            // Obtaining the texture sizes
+            var textureRedChannelWidth = textureRedChannel.width;
+            var textureRedChannelHeight = textureRedChannel.height;
+            var textureGreenChannelWidth = textureGreenChannel.width;
+            var textureGreenChannelHeight = textureGreenChannel.height;
+            var textureBlueChannelWidth = textureBlueChannel.width;
+            var textureBlueChannelHeight = textureBlueChannel.height;
+            
+            // If the textures are the same size, combine them...
+            if ((textureRedChannelWidth == textureGreenChannelWidth && 
+                 textureRedChannelWidth == textureBlueChannelWidth) &&
+                (textureRedChannelHeight == textureGreenChannelHeight && 
+                 textureRedChannelHeight == textureBlueChannelHeight))
+            {
+                packedTexture = CreatePackedTexture(
+                    textureRedChannelWidth, textureRedChannelHeight,
+                    textureRedChannel,
+                    textureGreenChannel,
+                    textureBlueChannel,
+                    compositeTextureName);
+
+                return packedTexture;
+            }
+            else
+            {
+                throw new System.Exception("The input textures need to be the same size.");
+            }
+            
+            // TODO: ... if the textures are not the same size ...
+            // TODO: ... find the smallest size, reduce the others and combine them
+            
+        }
+
+        private static UnityEngine.Texture2D CreatePackedTexture(
+            int width, int height,
+            Texture2D textureRedChannel,
+            Texture2D textureGreenChannel,
+            Texture2D textureBlueChannel,
+            string compositeTextureName)
+        {
+            var packedTexture = new Texture2D(width, height);
+            packedTexture.name = compositeTextureName;
+
+            var rwCopyTextureRedChannel = TextureUtilities.GetRwTextureCopy(textureRedChannel);
+            var rwCopyTextureGreenChannel = TextureUtilities.GetRwTextureCopy(textureGreenChannel);
+            var rwCopyTextureBlueChannel = TextureUtilities.GetRwTextureCopy(textureBlueChannel);
+
+            for (var i = 0; i < width; i++)
+            {
+                for (var j = 0; j < height; j++)
+                {
+                    packedTexture.SetPixel(
+                        i, j,
+                        new Color(
+                            rwCopyTextureRedChannel.GetPixel(i, j).grayscale,
+                            rwCopyTextureGreenChannel.GetPixel(i, j).grayscale,
+                            rwCopyTextureBlueChannel.GetPixel(i, j).grayscale));
+                }
+            }
+            
+            return packedTexture;
+        }
+    }
+}
+
+
---
+++ Editor/TextureUtilities.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+namespace UnityTextureRgbPacker.Editor
+{
+    public class TextureUtilities
+    {
+        public enum TextureUtilitiesFormats
+        {
+            Png,
+            Tga
+        }
+        
+        public static void SaveTextureToPath(Texture2D texture, string path, TextureUtilitiesFormats format)
+        {
+            var bytes = new byte[0];
+
+            switch (format)
+            {
+                case TextureUtilitiesFormats.Png:
+                    bytes = texture.EncodeToPNG();
+                    break;
+                case TextureUtilitiesFormats.Tga:
+                    bytes = texture.EncodeToTGA();
+                    break;
+                default:
+                    break;
+            }
+
+            System.IO.File.WriteAllBytes(path, bytes);
+        }
+        
+        public static Texture2D GetRwTextureCopy(Texture2D sourceTexture)
+        {
+            var renderTexture = RenderTexture.GetTemporary(
+                sourceTexture.width,
+                sourceTexture.height,
+                0,
+                RenderTextureFormat.Default,
+                RenderTextureReadWrite.Linear);
+ 
+            Graphics.Blit(sourceTexture, renderTexture);
+            var previous = RenderTexture.active;
+            RenderTexture.active = renderTexture;
+            var readableText = new Texture2D(sourceTexture.width, sourceTexture.height);
+            readableText.ReadPixels(new Rect(0, 0, renderTexture.width, renderTexture.height), 0, 0);
+            readableText.Apply();
+            RenderTexture.active = previous;
+            RenderTexture.ReleaseTemporary(renderTexture);
+            return readableText;
+        }
+    }
+}
